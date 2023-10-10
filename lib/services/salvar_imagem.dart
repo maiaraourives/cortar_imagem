44fb033aa2f3190429a24d8626bf0a7fa8e29d6a
Maiara Ferreira <103464615+MaiaFerreira04@users.noreply.github.com>
2023-10-10 16:09:55 -0300

    Ajuste

--- lib/services/salvar_imagem.dart
+++ lib/services/salvar_imagem.dart
@@ -1,9 +1,17 @@
 import 'dart:io';
 
+import 'dart:io' as io;
+
 class SalvarImagem{
 
   static Future<String> salvarImagem(File file) async {
-    
+
+    // para um arquivo
+    io.File(file.path).exists();
+
+    // // para um diretório
+    // io.Directory(await path).exists();
+
     String filename = 'imagem - ${DateTime.now().microsecondsSinceEpoch.toString()}.jpg';
 
     String savedPath = '$filename';
@@ -11,7 +19,5 @@ class SalvarImagem{
     File(file.path);
 
     return savedPath;
-
   }
-
 }
