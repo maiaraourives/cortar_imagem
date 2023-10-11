857aebadc0196ea983bba3047d2d13d3251a66b5
Maiara Ferreira <103464615+MaiaFerreira04@users.noreply.github.com>
2023-10-11 09:03:40 -0300

    Ajuste

--- lib/services/salvar_imagem.dart
+++ lib/services/salvar_imagem.dart
@@ -1,21 +1,18 @@
 import 'dart:io';
-
-import 'package:path_provider/path_provider.dart';
-
+import 'package:file_picker/file_picker.dart';
 class SalvarImagem{
 
   static Future<String> salvarImagem(File file) async {
 
-    Directory directory = Directory('C:/Users/maiara.ferreira/Documents');
-    
-    if (!await directory.exists()) directory = (await getExternalStorageDirectory())!;
-
     String filename = 'imagem - ${DateTime.now().microsecondsSinceEpoch.toString()}.jpg';
 
-    String savedPath = '$filename';
+    String? outputFile = await FilePicker.platform.saveFile(
+      dialogTitle: 'Salve seu arquivo no local desejado',
+      fileName: filename
+    );
 
-    File(file.path);
+    File(outputFile!).writeAsBytesSync(file.readAsBytesSync());
 
-    return savedPath;
+    return outputFile;
   }
 }
